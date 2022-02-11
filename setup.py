24dd5692de98edcf6edc349a2b5a9f611f1cafec
SomeAuthor123 <99427702+SomeAuthor123@users.noreply.github.com>
2022-02-11 08:59:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='cc_model',
     version='0.1.0',
     packages=['cc_model'],
-    author='Felix Stamm',
-    author_email='felix.stamm@cssh.rwth-aachen.de',
+    author='SomeOne',
+    author_email='some_mail@some_where.com',
     description='This package enables the colorful-configuration model for python',
     install_requires=[
               'pandas', 'scipy', 'numpy', 'matplotlib', 'numba', 'networkx'
